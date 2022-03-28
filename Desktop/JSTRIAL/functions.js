c901530a16ecdbec3b86ca6679f2cf40e13e3e92
devotagift <devotaingabire906@gmail.com>
2022-03-28 12:42:18 +0300

    temparaure gauge

---
+++ Desktop/JSTRIAL/functions.js
@@ -0,0 +1,10 @@
+let age = 13;
+function checkAge(){
+    let age = 15;
+    if( age>= 16){
+        console.log ('you may attend');
+    }
+    else {
+        console.log ('sorry, we cant let you in.')
+    }
+}
---
+++ Desktop/JSTRIAL/temp.js
@@ -0,0 +1,10 @@
+const TEMP =38;
+        if(TEMP>=20 && TEMP <=36){
+            console.log('your temperature is normal, your are healthy')}
+            else if(TEMP>=37 && TEMP <=40){
+                console.log('your temperature is above the normal,visit your doctor soon')
+            }
+            else{
+                console.log('your temperature is erratic, visit the nearest hospital please')
+            }
+
